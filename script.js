8690f41c1e24d81a625d13ac8b6808dc8b382838
Taylor <153626476+Taylor-Carr@users.noreply.github.com>
2025-11-09 23:49:31 +0000

    mobile responsive changes

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+  const burger = document.getElementById('burger');
+  const navLinks = document.getElementById('nav-links');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navLinks.classList.toggle('show');
+  });
+
 // Typing Animation for "Media" -> "Marketing"
 document.addEventListener("DOMContentLoaded", function () {
   const words = ["Media..", "Marketing..", "Management.."];
